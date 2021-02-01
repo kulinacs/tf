84d0b67c535d808414479cfee5e4c3b5c6847c50
Nicklaus McClendon <nicklaus@kulinacs.com>
2021-02-01 17:08:20 -0600

    Massive update

--- kube02/main.tf
+++ kube02/main.tf
@@ -6,16 +6,15 @@ module "centos8" {
   source = "../modules/centos"
 
   base_volume_id = "/mnt/volumes/centos8.qcow2"
-  name           = "storage"
+  name           = "kube02"
   tag            = "lab"
   username       = "kulinacs"
   network_name   = "lab"
   pool           = "primary"
   ssh_key        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOP78FDtl87/nxkEtcq9vepcyV2w2bcrn59G7BGnr73E kulinacs@bernstein"
-  address        = "10.0.0.3/24"
+  address        = "10.0.0.11/24"
   gateway        = "10.0.0.1"
-  vcpus          = 4
-  memory         = "8192"
+  vcpus          = 2
+  memory         = "8196"
   ovs_networks   = ["lab"]
-  block_devices  = ["/dev/sda1", "/dev/sdd1"]
 }
